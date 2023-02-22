fd526cce9cedadbc91a9d338eeef74268554c3be
francesco <info.francescobianco@gmail.com>
2023-02-22 14:18:15 +0100

    First commit

--- crontab-entrypoint.sh
+++ crontab-entrypoint.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
->/var/spool/cron/crontabs/root
+crontab=/var/spool/cron/crontabs/root
+
+>$crontab
 
 if [[ "$1" =~ ^[1-9*] ]]; then
   while test $# -gt 0; do
-      echo "$1" >> /var/spool/cron/crontabs/root
+      echo "$1" >> $crontab
       shift
   done
   set -- crond -f -L /dev/stdout -l 8
 fi
 
 if [ -f /etc/crontab ]; then
-  cat /etc/crontab >> /var/spool/cron/crontabs/root
+  cat /etc/crontab >> $crontab
 fi
 
 docker-entrypoint.sh "$@"
